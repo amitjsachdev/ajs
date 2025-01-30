37b8356adc4687d758afaaa5c8fa3a97c60edf74
amitjsachdev <amitjsachdev@gmail.com>
2025-01-30 11:07:26 +0530

    Update ajs.js

--- ajs.js
+++ ajs.js
@@ -1,1 +1,14 @@
-console.log("i used to love her")
+console.log("i used to love her ... worst part is i still do ... i dont have an option");
+// Chai aur Code 
+let accountId = 12345
+let accountEmail = "amit@gmail.com"
+let accountPassword = "123456"
+let accountCity = "mumbai"
+
+// accountId = 2 
+
+accountEmail = "hc"
+accountPassword = "098"
+accountCity = "Jaipur"
+ 
+console.table([accountId, accountEmail, accountPassword, accountCity]); 
